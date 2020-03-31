6a36fdabcc03272836c95774c660adece73cb9c8
Chris Parnin <chris.parnin@gmail.com>
2020-03-31 13:08:54 -0400

    trim

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -4,7 +4,7 @@ const path = require('path');
 const os = require('os');
 
 exports.command = 'shutdown';
-exports.desc = 'Provision and configure the configuration server';
+exports.desc = 'Shutdown the servers';
 exports.builder = yargs => {
 };
 
@@ -30,16 +30,5 @@ async function run() {
     console.log(chalk.blueBright('Provisioning green server...'));
     result = child.spawnSync(`bakerx`, `delete vm green`.split(' '), {shell:true, stdio: 'inherit'} );
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
-
-    // console.log(chalk.blueBright('Installing privateKey on blue server'));
-    // let identifyFile = privateKey || path.join(os.homedir(), '.bakerx', 'insecure_private_key');
-    // result = scpSync (identifyFile, 'vagrant@192.168.33.10:/home/vagrant/.ssh/mm_rsa');
-    // if( result.error ) { console.log(result.error); process.exit( result.status ); }
-
-    // console.log(chalk.blueBright('Running init script...'));
-    // result = sshSync('/bakerx/cm/server-init.sh', 'vagrant@192.168.33.10');
-    // if( result.error ) { console.log(result.error); process.exit( result.status ); }
-
-
 
 }
