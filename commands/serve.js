2c0e634c51ac6496a40363c1b09bddd1602508bd
Chris Parnin <chris.parnin@gmail.com>
2020-03-31 13:10:44 -0400

    initial serve

---
+++ commands/serve.js
@@ -0,0 +1,76 @@
+const chalk = require('chalk');
+const path = require('path');
+const os = require('os');
+
+const got = require('got');
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+exports.command = 'serve';
+exports.desc = 'Run traffic proxy.';
+exports.builder = yargs => {};
+
+exports.handler = async argv => {
+    const { } = argv;
+
+    (async () => {
+
+        await run( );
+
+    })();
+
+};
+
+const BLUE  = 'http://192.168.44.25:3000';
+const GREEN = 'http://192.168.44.30:3000';
+
+class Production
+{
+    constructor()
+    {
+        this.TARGET = GREEN;
+        setInterval( this.healthCheck.bind(this), 5000 );
+    }
+
+    // TASK 1: 
+    proxy()
+    {
+        let options = {};
+        let proxy   = httpProxy.createProxyServer(options);
+        let self = this;
+        // Redirect requests to the active TARGET (BLUE or GREEN)
+        let server  = http.createServer(function(req, res)
+        {
+            // callback for redirecting requests.
+        });
+        server.listen(3080);
+   }
+
+   failover()
+   {
+      this.TARGET = BLUE;
+   }
+
+   async healthCheck()
+   {
+      try 
+      {
+         const response = await got(this.TARGET);
+         let status = response.statusCode == 200 ? chalk.green(response.statusCode) : chalk.red(response.statusCode);
+         console.log( chalk`{grey Health check on ${this.TARGET}}: ${status}`);
+      }
+      catch (error) {
+         console.log(error);
+      }
+   }
+   
+}
+
+async function run() {
+
+    console.log(chalk.keyword('pink')('Starting proxy on localhost:3080'));
+
+    let prod = new Production();
+    prod.proxy();
+
+}
